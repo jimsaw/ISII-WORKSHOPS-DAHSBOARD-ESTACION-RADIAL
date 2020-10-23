74df970a5ef8ecc2c4390e5305cba5faf5b18fe5
MelanieLasso <mtlasso@espol.edu.ec>
2020-10-22 21:36:33 -0500

    Creation of getters y setters

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -1,7 +1,31 @@
 package ec.edu.espol.workshops.second;
 
 public class CarInsurance {
+	
 	private int edad;
-	private String texto;
+	private String sexo;
 	private String estadoCivil;
+	
+	
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public String getEstadoCivil() {
+		return estadoCivil;
+	}
+	public void setEstadoCivil(String estadoCivil) {
+		this.estadoCivil = estadoCivil;
+	}
+	
+	
 }
+

cbf707eb3a3aa67bbb32363a1d77a1b5c28d3a0f
Luis Montenegro <lfmonten@espol.edu.ec>
2020-11-20 10:08:00 -0500

    Correct the code(Programmer)

--- ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
+++ ec.edu.espol.workshops.second/src/main/java/ec/edu/espol/workshops/second/CarInsurance.java
@@ -11,17 +11,36 @@ public class CarInsurance {
 	
 	
 	public CarInsurance(String edad, String sexo, String isMarried, String ci) { //Class of a Car Insurance
-		this.edad = Integer.parseInt(edad);
-		this.sexo = sexo;
-
-		if(isMarried.equals("y") || isMarried.equals("yes") || isMarried.equals("YES")  || isMarried.equals("Yes")) {
-
-			this.isMarried = true;	
-		} else {
-			this.isMarried = false;
-		}
-		this.ci = ci;
+		int validator = validateage(edad);
 		
+		if(validator == 1) {
+		
+			this.edad = Integer.parseInt(edad);
+			this.sexo = sexo;
+	
+			if(isMarried.equals("y") || isMarried.equals("yes") || isMarried.equals("YES")  || isMarried.equals("Yes")) {
+	
+				this.isMarried = true;	
+			} else {
+				this.isMarried = false;
+			}
+			this.ci = ci;
+		}else {
+			this.edad = 0;
+			this.sexo = "";
+			this.isMarried = false;
+			this.ci = "";
+		}
+		
+	}
+	
+	public int validateage(String age) {
+		try {
+		    Integer.parseInt(age);
+		} catch (NumberFormatException e) {
+		    return -1;
+		}
+		return 1;
 	}
 	
 	public int getEdad() {
@@ -64,6 +83,14 @@ public class CarInsurance {
 	
 	public int calculatePremium() { //Method to calculate the premium value
 		int base = 500;
+		if (!(this.sexo.equals("F")) && !(this.sexo.equals("M"))) {
+			JOptionPane.showMessageDialog(null, "Sexo ingresado no es valido");
+			return -1;
+		}
+		if (this.edad == 0) {
+			JOptionPane.showMessageDialog(null, "Edad ingresada no es valida, utilice un numero");
+			return -1;
+		}
 		if (this.edad > 80) {
 			JOptionPane.showMessageDialog(null, "Edad no es permitida para otorgarle un seguro");
 			return -1;
@@ -92,6 +119,13 @@ public class CarInsurance {
 		sexo = JOptionPane.showInputDialog("Ingrese su sexo(F/M): ");
 		isMarried = JOptionPane.showInputDialog("Se encuentra casado(a) (y/n): ");
 		ci = JOptionPane.showInputDialog("Ingrese su n�mero de c�dula: ");	
+		
+		String errmsg;
+		if(!isMarried.equals("y") && !isMarried.equals("n")) {
+			errmsg = "Estado civil ingresado no valido";
+			JOptionPane.showMessageDialog(null, errmsg);
+			System.exit(0);  
+		}
 		
 		CarInsurance cInsurance = new CarInsurance(edad, sexo, isMarried, ci);
 		
